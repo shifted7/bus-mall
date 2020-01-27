d67b66be7acb2bb684453432b37edfc985a6a98c
Andrew <shifted6@gmail.com>
2020-01-26 18:17:02 -0800

    js now specifies img paths without initial slash, for github deployment.

--- app.js
+++ app.js
@@ -229,26 +229,26 @@ function reportProductResults(parentElement) {
 
 Product.allProducts = [];
 // Initialize all products
-new Product('Star Wars Luggage Bag', '/img/bag.jpg');
-new Product('Banana Slicer', '/img/banana.jpg');
-new Product('Open-toe Rainboots', '/img/boots.jpg');
-new Product('Bathroom Stand', '/img/bathroom.jpg');
-new Product('Breakfast Maker', '/img/breakfast.jpg');
-new Product('Meatball Bubblegum', '/img/bubblegum.jpg');
-new Product('Curved Chair', '/img/chair.jpg');
-new Product('Cthulhu Toy', '/img/cthulhu.jpg');
-new Product('Dog Duck Mask', '/img/dog-duck.jpg');
-new Product('Dragon Meat', '/img/dragon.jpg');
-new Product('Utensil Pen', '/img/pen.jpg');
-new Product('Pet Sweeper', '/img/pet-sweep.jpg');
-new Product('Pizza Scissors', '/img/scissors.jpg');
-new Product('Shark Sleeping Bag', '/img/shark.jpg');
-new Product('Baby Sweeper', '/img/sweep.png');
-new Product('Tauntaun Sleeping Bag', '/img/tauntaun.jpg');
-new Product('Unicorn Meat', '/img/unicorn.jpg');
-new Product('Tentacle USB Drive', '/img/usb.gif');
-new Product('Twisted Watering Can', '/img/water-can.jpg');
-new Product('Twisted Wine Glass', '/img/wine-glass.jpg');
+new Product('Star Wars Luggage Bag', 'img/bag.jpg');
+new Product('Banana Slicer', 'img/banana.jpg');
+new Product('Open-toe Rainboots', 'img/boots.jpg');
+new Product('Bathroom Stand', 'img/bathroom.jpg');
+new Product('Breakfast Maker', 'img/breakfast.jpg');
+new Product('Meatball Bubblegum', 'img/bubblegum.jpg');
+new Product('Curved Chair', 'img/chair.jpg');
+new Product('Cthulhu Toy', 'img/cthulhu.jpg');
+new Product('Dog Duck Mask', 'img/dog-duck.jpg');
+new Product('Dragon Meat', 'img/dragon.jpg');
+new Product('Utensil Pen', 'img/pen.jpg');
+new Product('Pet Sweeper', 'img/pet-sweep.jpg');
+new Product('Pizza Scissors', 'img/scissors.jpg');
+new Product('Shark Sleeping Bag', 'img/shark.jpg');
+new Product('Baby Sweeper', 'img/sweep.png');
+new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
+new Product('Unicorn Meat', 'img/unicorn.jpg');
+new Product('Tentacle USB Drive', 'img/usb.gif');
+new Product('Twisted Watering Can', 'img/water-can.jpg');
+new Product('Twisted Wine Glass', 'img/wine-glass.jpg');
 
 getStoredProductData();
 
